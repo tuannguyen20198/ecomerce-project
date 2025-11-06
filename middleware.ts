61f2f62a0ccee945a4f5eca5cb2210678b74a1ff
tuannguyen20198 <lntt20198@gmail.com>
2025-11-06 19:53:43 +0700

    Implement middleware for authentication and session handling

--- middleware.ts
+++ middleware.ts
@@ -1,1 +1,17 @@
-export { auth as middleware } from "@/auth";
+import { auth } from "@/auth";
+import { NextRequest, NextResponse } from "next/server";
+
+export default async function middleware(req: NextRequest) {
+  const session = await auth();
+  
+  if (!session) {
+    return NextResponse.redirect(new URL('/login', req.url));
+  }
+
+  return NextResponse.next();
+}
+
+export const config = {
+    matcher: ['/((?!_next/static|_next/image|favicon.ico).*)'],
+    runtime: 'nodejs', // ⚠️ không còn Edge nữa
+};
